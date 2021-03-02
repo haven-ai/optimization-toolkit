288674e918c03d44bf0714d08d8477ea6eabd828
Xinhong Deng <sandra.deng.0520@gmail.com>
2021-03-02 17:20:38 -0500

    update exp config

--- exp_configs/__init__.py
+++ exp_configs/__init__.py
@@ -1,6 +1,6 @@
-from . import adaptive_exps, sps_exps
+from . import adaptive_exps, sps_exps, benchmark_exps
 
 EXP_GROUPS = {}
 EXP_GROUPS.update(adaptive_exps.EXP_GROUPS)
 EXP_GROUPS.update(sps_exps.EXP_GROUPS)
-
+EXP_GROUPS.update(benchmark_exps.EXP_GROUPS)
